8dfa1d68e56245f4df6bba5abfb8a213cd3b4df0
ayub-a <ayub-dev@yandex.ru>
2025-02-11 20:52:18 +0500

    feat(icon): new icon variants, svg files

--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -6,6 +6,8 @@ import burger from 'shared/assets/icons/burger.svg'
 import wholesale from 'shared/assets/icons/wholesale.svg'
 import arrowRight from 'shared/assets/icons/arrowRight.svg'
 import localeTime from 'shared/assets/icons/localeTime.svg'
+import emptyCategory from 'shared/assets/icons/emptyCategory.svg'
+import search from 'shared/assets/icons/search.svg'
 
 // main nav
 import notification from 'shared/assets/icons/notification.svg'
@@ -13,9 +15,12 @@ import favourites from 'shared/assets/icons/favourite.svg'
 import cart from 'shared/assets/icons/cart.svg'
 import user from 'shared/assets/icons/user.svg'
 
+import { clsnm } from 'shared/lib/classNames'
+
 import cls from './Icon.module.scss'
 
 const icons = {
+	search,
 	location,
 	language,
 	localeTime,
@@ -24,22 +29,25 @@ const icons = {
 	wholesale,
 	arrowRight,
 	notification,
-	favourites,
 	cart,
 	user,
+	emptyCategory,
+	favourites,
+	like: favourites,
 }
 
 export interface IconProps {
 	name: keyof typeof icons
 	size?: number
 	color?: string
+	className?: string
 }
 
-export const Icon = ({ name, size = 19, color = 'currentColor' }: IconProps) => {
+export const Icon = ({ name, size = 19, color = 'currentColor', className }: IconProps) => {
 	const icon = name ? icons[name] : null
 
 	return (
-		<svg className={cls.Icon} data-icon={name} width={size} height={size} fill={color}>
+		<svg className={clsnm(cls.Icon, [className])} data-icon={name} width={size} height={size} fill={color}>
 			<use xlinkHref={`/icons.svg#${icon.id}`} />
 		</svg>
 	)
