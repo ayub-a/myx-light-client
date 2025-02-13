37ab638026fbfe1b531abdc3f18da810f6dfd4e1
ayub-a <ayub-dev@yandex.ru>
2025-02-13 06:18:01 +0500

    feat(app-layout): add AppLayout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,91 +1,103 @@
 import { useTheme } from 'shared/lib/theme'
-import { Navbar } from 'widgets/Navbar/ui/Navbar'
 import { clsnm } from 'shared/lib/classNames'
 
+import { AppLayout } from './layout/AppLayout/AppLayout'
 import { AppRouter } from './router'
 
 import './styles/index.scss'
-import { Button } from 'shared/ui'
 
 export const App = () => {
-	const { theme, toggleTheme } = useTheme()
+	const { theme } = useTheme()
 
 	return (
-		<div className={clsnm('App', [`theme_${theme}`])}>
-			<Navbar />
-
-			{/* <MainNavigation />
-			<CategoryNavigation /> */}
-
-			{/* <button onClick={toggleTheme}>Theme: {theme}</button> */}
-
+		<AppLayout className={clsnm('App', [`theme_${theme}`])}>
 			<AppRouter />
-
-			{/* <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '10px' }}>
-				<h3>BTN STYLES | Default btn size: M</h3>
-				<div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-					<Button style='clear'>Clear</Button>
-					<Button style='rounded'>Rounded</Button>
-					<Button style='outline-rounded'>Outline Rounded</Button>
-					<Button style='square'>Square</Button>
-					<Button style='outline-square'>Outline Square</Button>
-				</div>
-
-				<h3>BTN SIZES</h3>
-				<div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-					<Button style='clear' size='s'>
-						Clear | S
-					</Button>
-					<Button style='rounded' size='s'>
-						Rounded | S
-					</Button>
-					<Button style='outline-rounded' size='s'>
-						Outline Rounded | S
-					</Button>
-					<Button style='square' size='s'>
-						Square | S
-					</Button>
-					<Button style='outline-square' size='s'>
-						Outline Square | S
-					</Button>
-				</div>
-
-				<div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-					<Button style='clear' size='m'>
-						Clear | M
-					</Button>
-					<Button style='rounded' size='m'>
-						Rounded | M
-					</Button>
-					<Button style='outline-rounded' size='m'>
-						Outline Rounded | M
-					</Button>
-					<Button style='square' size='m'>
-						Square | M
-					</Button>
-					<Button style='outline-square' size='m'>
-						Outline Square | M
-					</Button>
-				</div>
-
-				<div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-					<Button style='clear' size='l'>
-						Clear | L
-					</Button>
-					<Button style='rounded' size='l'>
-						Rounded | L
-					</Button>
-					<Button style='outline-rounded' size='l'>
-						Outline Rounded | L
-					</Button>
-					<Button style='square' size='l'>
-						Square | L
-					</Button>
-					<Button style='outline-square' size='l'>
-						Outline Square | L
-					</Button>
-				</div>
-			</div> */}
-		</div>
+		</AppLayout>
 	)
+}
+
+{
+	/* <div > */
+}
+
+{
+	/* <MainNavigation /> */
+}
+{
+	/* <CategoryNavigation /> */
+}
+
+{
+	/* <button onClick={toggleTheme}>Theme: {theme}</button> */
+}
+
+{
+	/* <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '10px' }}>
+		<h3>BTN STYLES | Default btn size: M</h3>
+		<div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+			<Button style='clear'>Clear</Button>
+			<Button style='rounded'>Rounded</Button>
+			<Button style='outline-rounded'>Outline Rounded</Button>
+			<Button style='square'>Square</Button>
+			<Button style='outline-square'>Outline Square</Button>
+		</div>
+
+		<h3>BTN SIZES</h3>
+		<div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+			<Button style='clear' size='s'>
+				Clear | S
+			</Button>
+			<Button style='rounded' size='s'>
+				Rounded | S
+			</Button>
+			<Button style='outline-rounded' size='s'>
+				Outline Rounded | S
+			</Button>
+			<Button style='square' size='s'>
+				Square | S
+			</Button>
+			<Button style='outline-square' size='s'>
+				Outline Square | S
+			</Button>
+		</div>
+
+		<div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+			<Button style='clear' size='m'>
+				Clear | M
+			</Button>
+			<Button style='rounded' size='m'>
+				Rounded | M
+			</Button>
+			<Button style='outline-rounded' size='m'>
+				Outline Rounded | M
+			</Button>
+			<Button style='square' size='m'>
+				Square | M
+			</Button>
+			<Button style='outline-square' size='m'>
+				Outline Square | M
+			</Button>
+		</div>
+
+		<div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+			<Button style='clear' size='l'>
+				Clear | L
+			</Button>
+			<Button style='rounded' size='l'>
+				Rounded | L
+			</Button>
+			<Button style='outline-rounded' size='l'>
+				Outline Rounded | L
+			</Button>
+			<Button style='square' size='l'>
+				Square | L
+			</Button>
+			<Button style='outline-square' size='l'>
+				Outline Square | L
+			</Button>
+		</div>
+	</div> */
+}
+{
+	/* </div> */
 }
---
+++ src/app/layout/AppLayout/AppLayout.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from 'react'
+
+import { Navbar } from 'widgets/Navbar/ui/Navbar'
+import { clsnm } from 'shared/lib/classNames'
+
+import cls from './AppLayout.module.scss'
+
+interface AppLayoutProps {
+	children: ReactNode
+	className?: string
+}
+
+export const AppLayout = ({ children, className }: AppLayoutProps) => {
+	return (
+		<div className={clsnm(cls.AppLayout, [className])}>
+			<Navbar />
+
+			<div className={cls.container}>{children}</div>
+
+			{/* <Foote/> */}
+		</div>
+	)
+}
