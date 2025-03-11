db05ce9d7f5eaac1b665e19fd496dafbb0462da4
ayub-a <ayub-dev@yandex.ru>
2025-03-11 10:34:39 +0500

    refactor(ui): update CarModel page

--- src/pages/CarModel/CarModel.tsx
+++ src/pages/CarModel/CarModel.tsx
@@ -1,6 +1,15 @@
 import { useLocation, useNavigate } from 'react-router-dom'
 
-const CarModel = () => {
+import { PropsWithChildren } from 'react'
+
+import { clsnm } from 'shared/lib/classNames'
+
+import cls from './CarModel.module.scss'
+import { GridLayout } from 'shared/ui'
+
+interface CarModelProps {}
+
+const CarModel = (props: PropsWithChildren<CarModelProps>) => {
 	const navigate = useNavigate()
 	const { pathname } = useLocation()
 
@@ -9,9 +18,28 @@ const CarModel = () => {
 	}
 
 	return (
-		<div>
-			<h2>CAR MODEL</h2>
-			<button onClick={() => handleModelSelect('A3')}>A3</button>
+		<div className={clsnm(cls.CarModel, [], {})}>
+			<div className={cls.umbrella}></div>
+
+			<div className={cls.page_header}>
+				<h3>Выберите модель</h3>
+			</div>
+
+			<GridLayout>
+				{new Array(15).fill('A').map((model, i) => (
+					<div
+						key={i}
+						className={cls.model_item}
+						data-model={`${model}${i + 1}`}
+						onClick={() => handleModelSelect(`${model}${i + 1}`)}
+					>
+						<span className={cls.model_title}>
+							{model}
+							{i + 1}
+						</span>
+					</div>
+				))}
+			</GridLayout>
 		</div>
 	)
 }
