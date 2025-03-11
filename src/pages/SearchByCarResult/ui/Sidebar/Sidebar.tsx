99ede3a1c137e06195ac6a05487266137a2f13c1
ayub-a <ayub-dev@yandex.ru>
2025-03-11 17:35:58 +0500

    feat(ui): add SearchByCarResult Sidebar

---
+++ src/pages/SearchByCarResult/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,56 @@
+import { PropsWithChildren } from 'react'
+
+import { clsnm } from 'shared/lib/classNames'
+
+import cls from './Sidebar.module.scss'
+
+import coveredCar from 'shared/assets/carBrands/covered-car.png'
+import audiCar from 'shared/assets/carBrands/audi-car.png'
+import audiLogo from 'shared/assets/carBrands/audi-logo.png'
+import { useParams } from 'react-router-dom'
+
+interface SidebarProps {}
+
+const aboutCar = [
+	['Поколение', ''],
+	['Класс', 'Gran Turismo'],
+	['Год', '2013-2020'],
+	['Код кузова', 'F34'],
+	['Двери', '3'],
+]
+
+export const Sidebar = (props: PropsWithChildren<SidebarProps>) => {
+	const { brand, model, body, gen } = useParams()
+
+	return (
+		<div className={clsnm(cls.Sidebar, [], {})}>
+			<div className={cls.car_and_logo}>
+				<div className={cls.car_logo}>
+					<img src={audiLogo} alt='' />
+				</div>
+				<div className={cls.car_img}>
+					<img src={audiCar} alt='' />
+				</div>
+			</div>
+
+			<div className={cls.about_car}>
+				<div className={cls.brand_model_body}>
+					<h3>
+						{brand} {model}
+					</h3>
+					<span>{body}</span>
+				</div>
+
+				<ul className={cls.about_car_list}>
+					{aboutCar.map((car, i) => (
+						<li key={i} className={cls.about_item}>
+							<div className={cls.title}>{car[0]}</div>
+							<span className={cls.divider}></span>
+							<div className={cls.value}>{car[1] ? car[1] : gen}</div>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	)
+}
