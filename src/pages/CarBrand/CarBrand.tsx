0e7733955af5d5a48d30687f62006d40cd81e3a5
ayub-a <ayub-dev@yandex.ru>
2025-03-11 10:11:31 +0500

    refactor(ui): update CarBrand Page

--- src/pages/CarBrand/CarBrand.tsx
+++ src/pages/CarBrand/CarBrand.tsx
@@ -1,6 +1,17 @@
 import { useNavigate } from 'react-router-dom'
+import { GridLayout, Icon } from 'shared/ui'
 
-const CarBrand = () => {
+import { PropsWithChildren } from 'react'
+
+import { clsnm } from 'shared/lib/classNames'
+
+import cls from './CarBrand.module.scss'
+
+import audiLogo from 'shared/assets/carBrands/audi-logo.png'
+
+interface CarBrandProps {}
+
+const CarBrand = (props: PropsWithChildren<CarBrandProps>) => {
 	const navigate = useNavigate()
 
 	const handleBrandSelect = (brand: string) => {
@@ -8,9 +19,25 @@ const CarBrand = () => {
 	}
 
 	return (
-		<div>
-			<h2>CAR BRAND</h2>
-			<button onClick={() => handleBrandSelect('Audi')}>Audi</button>
+		<div className={clsnm(cls.CarBrand, [], {})}>
+			<div className={cls.umbrella}></div>
+
+			<div className={cls.page_header}>
+				<h3>Выберите бренд</h3>
+
+				<div className={cls.brand_search}>
+					<input type='text' placeholder='Поиск бренда' />
+					<Icon name='search' />
+				</div>
+			</div>
+
+			<GridLayout>
+				{new Array(15).fill('Audi').map((brand, i) => (
+					<div key={i} className={cls.brand_item} onClick={() => handleBrandSelect(brand)}>
+						<img src={audiLogo} alt={brand} />
+					</div>
+				))}
+			</GridLayout>
 		</div>
 	)
 }
