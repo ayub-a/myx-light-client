5b63e00cadcb3191b5b8e002aa6c069109320cae
ayub-a <ayub-dev@yandex.ru>
2025-03-11 20:08:42 +0500

    refactor(car-brand): add Brand logos, update styles

--- src/pages/CarBrand/CarBrand.tsx
+++ src/pages/CarBrand/CarBrand.tsx
@@ -7,9 +7,57 @@ import { clsnm } from 'shared/lib/classNames'
 
 import cls from './CarBrand.module.scss'
 
-import audiLogo from 'shared/assets/carBrands/audi-logo.png'
+import brandChevrolet from 'shared/assets/carBrands/brand-chevrolet.png'
+import brandHyundai from 'shared/assets/carBrands/brand-hyundai.png'
+import brandToyota from 'shared/assets/carBrands/brand-toyota.png'
+import brandKia from 'shared/assets/carBrands/brand-kia.png'
+import brandByd from 'shared/assets/carBrands/brand-byd.png'
+import brandVolkswagen from 'shared/assets/carBrands/brand-volkswagen.png'
+import brandMercedes from 'shared/assets/carBrands/brand-mercedes.png'
+import brandBmw from 'shared/assets/carBrands/brand-bmw.png'
+import brandNissan from 'shared/assets/carBrands/brand-nissan.png'
+import brandMazda from 'shared/assets/carBrands/brand-mazda.png'
+import brandRenault from 'shared/assets/carBrands/brand-renault.png'
+import brandAudi from 'shared/assets/carBrands/brand-Audi.png'
+import brandSkoda from 'shared/assets/carBrands/brand-skoda.png'
+import brandLexus from 'shared/assets/carBrands/brand-lexus.png'
+import brandPeugeot from 'shared/assets/carBrands/brand-peugeot.png'
+import brandSeat from 'shared/assets/carBrands/brand-seat.png'
+import brandSuzuki from 'shared/assets/carBrands/brand-suzuki.png'
+import brandVolvo from 'shared/assets/carBrands/brand-volvo.png'
+import brandSubaru from 'shared/assets/carBrands/brand-subaru.png'
+import brandMitsubishi from 'shared/assets/carBrands/brand-mitsubishi.png'
+import brandLandRover from 'shared/assets/carBrands/brand-LandRover.png'
+import brandHonda from 'shared/assets/carBrands/brand-Honda.png'
+import brandDaewoo from 'shared/assets/carBrands/brand-Daewoo.png'
 
 interface CarBrandProps {}
+
+const brands = [
+	{ name: 'Chevrolet', icon: brandChevrolet },
+	{ name: 'Hyundai', icon: brandHyundai },
+	{ name: 'Toyota', icon: brandToyota },
+	{ name: 'Kia', icon: brandKia },
+	{ name: 'BYD', icon: brandByd },
+	{ name: 'Volkswagen', icon: brandVolkswagen },
+	{ name: 'Mercedes', icon: brandMercedes },
+	{ name: 'BMW', icon: brandBmw },
+	{ name: 'Nissan', icon: brandNissan },
+	{ name: 'Mazda', icon: brandMazda },
+	{ name: 'Renault', icon: brandRenault },
+	{ name: 'Audi', icon: brandAudi },
+	{ name: 'Skoda', icon: brandSkoda },
+	{ name: 'Lexus', icon: brandLexus },
+	{ name: 'Peugeot', icon: brandPeugeot },
+	{ name: 'Seat', icon: brandSeat },
+	{ name: 'Suzuki', icon: brandSuzuki },
+	{ name: 'Volvo', icon: brandVolvo },
+	{ name: 'Subaru', icon: brandSubaru },
+	{ name: 'Mitsubishi', icon: brandMitsubishi },
+	{ name: 'LandRover', icon: brandLandRover },
+	{ name: 'Honda', icon: brandHonda },
+	{ name: 'Daewoo', icon: brandDaewoo },
+]
 
 const CarBrand = (props: PropsWithChildren<CarBrandProps>) => {
 	const navigate = useNavigate()
@@ -32,9 +80,9 @@ const CarBrand = (props: PropsWithChildren<CarBrandProps>) => {
 			</div>
 
 			<GridLayout>
-				{new Array(15).fill('Audi').map((brand, i) => (
-					<div key={i} className={cls.brand_item} onClick={() => handleBrandSelect(brand)}>
-						<img src={audiLogo} alt={brand} />
+				{brands.map(({ name, icon }) => (
+					<div key={name} className={cls.brand_item} onClick={() => handleBrandSelect(name)}>
+						<img src={icon} alt='car brand' />
 					</div>
 				))}
 			</GridLayout>
