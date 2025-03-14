bc6f8e1a28db866d2101356b92c901475d7b0d5d
ayub-a <ayub-dev@yandex.ru>
2025-03-14 09:52:59 +0500

    feat(ui): add disabled cls to Button component

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, PropsWithChildren } from 'react'
+import { ButtonHTMLAttributes, memo, PropsWithChildren } from 'react'
 import { clsnm } from 'shared/lib/classNames'
 
 import cls from './Button.module.scss'
@@ -10,12 +10,15 @@ export interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement
 	size?: 's' | 'm' | 'l'
 }
 
-export const Button = (props: PropsWithChildren<ButtonProps>) => {
-	const { children = 'Children Empty', className = '', size = 'm', style = 'rounded', ...other } = props
+export const Button = memo((props: PropsWithChildren<ButtonProps>) => {
+	const { children = 'Children Empty', className = '', size = 'm', style = 'rounded', disabled, ...other } = props
 
 	return (
-		<button className={clsnm(cls.Button, [cls[style], cls[size], className])} {...other}>
+		<button
+			className={clsnm(cls.Button, [cls[style], cls[size], className], { [cls.disabled]: disabled })}
+			{...other}
+		>
 			{children}
 		</button>
 	)
-}
+})
