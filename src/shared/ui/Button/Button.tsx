9762794864d31f8b1e1e1b12cba3c17227a959e3
ayub-a <ayub-dev@yandex.ru>
2025-03-14 18:48:43 +0500

    feat(ui): update Locale component for i18n support

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,4 +1,4 @@
-import { ButtonHTMLAttributes, memo, PropsWithChildren } from 'react'
+import React, { ButtonHTMLAttributes, LegacyRef, memo, PropsWithChildren, RefAttributes } from 'react'
 import { clsnm } from 'shared/lib/classNames'
 
 import cls from './Button.module.scss'
@@ -8,13 +8,15 @@ type ButtonStyles = 'clear' | 'rounded' | 'square' | 'outline-rounded' | 'outlin
 export interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'style'> {
 	style?: ButtonStyles
 	size?: 's' | 'm' | 'l'
+	ref?: React.RefObject<HTMLButtonElement>
 }
 
-export const Button = memo((props: PropsWithChildren<ButtonProps>) => {
+export const Button = React.forwardRef<HTMLButtonElement, PropsWithChildren<ButtonProps>>((props, ref) => {
 	const { children = 'Children Empty', className = '', size = 'm', style = 'rounded', disabled, ...other } = props
 
 	return (
 		<button
+			ref={ref}
 			className={clsnm(cls.Button, [cls[style], cls[size], className], { [cls.disabled]: disabled })}
 			{...other}
 		>
--- src/widgets/Navbar/ui/Locale/Locale.tsx
+++ src/widgets/Navbar/ui/Locale/Locale.tsx
@@ -1,9 +1,48 @@
+import { useEffect, useRef, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
 import { Button, Icon } from 'shared/ui'
 import { clsnm } from 'shared/lib/classNames'
+import { LOCAL_STORAGE_LANG_KEY } from 'shared/const/localStorage'
 
 import cls from './Locale.module.scss'
 
+const languages: Record<string, { code: string; lang: string }> = {
+	ru: { code: 'ru', lang: 'Русский' },
+	en: { code: 'en', lang: 'English' },
+}
+
+const initialLanguage = localStorage.getItem(LOCAL_STORAGE_LANG_KEY) || languages.ru.code
+
 export const Locale = () => {
+	const { i18n } = useTranslation()
+	const [currentLang, setCurrentLang] = useState(initialLanguage)
+	const [showLangList, setShowLangList] = useState(false)
+
+	const langButtonRef = useRef<HTMLButtonElement>(null)
+
+	const changeLanguage = (code: string, lang: string) => {
+		i18n.changeLanguage(code)
+		setCurrentLang(languages[code].code)
+		localStorage.setItem(LOCAL_STORAGE_LANG_KEY, code)
+		setShowLangList(false)
+	}
+
+	const clickOutside = (e: MouseEvent) => {
+		if (langButtonRef.current && !langButtonRef.current.contains(e.target as Node)) {
+			setShowLangList(false)
+		}
+	}
+
+	useEffect(() => {
+		i18n.changeLanguage(initialLanguage)
+		document.body.addEventListener('click', clickOutside)
+
+		return () => {
+			document.body.removeEventListener('click', clickOutside)
+		}
+	}, [])
+
 	return (
 		<div className={clsnm(cls.Locale)}>
 			<Button style='clear' size='s'>
@@ -11,10 +50,24 @@ export const Locale = () => {
 				<span className={cls.text}>Uzbekistan</span>•<span>SUM</span>
 			</Button>
 
-			<Button style='clear' size='s'>
-				<Icon name='language' />
-				<span className={cls.text}>Russian</span>
-			</Button>
+			<div className={cls.language_section}>
+				<Button ref={langButtonRef} style='clear' size='s' onClick={() => setShowLangList((prev) => !prev)}>
+					<Icon name='language' />
+					<span className={cls.text}>{languages[currentLang].lang}</span>
+				</Button>
+
+				<ul className={clsnm(cls.language_list, [], { [cls.show_lang_list]: showLangList })}>
+					{Object.values(languages).map(({ code, lang }) => (
+						<li
+							key={code}
+							onClick={() => changeLanguage(code, lang)}
+							className={currentLang === code ? cls.active_lang : ''}
+						>
+							{lang}
+						</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	)
 }
