9cad2eb0dd363b165ce755b8b86ee0a8f7e7a750
ayub-a <ayub-dev@yandex.ru>
2025-03-18 18:23:39 +0500

    chore(jest): add Jest, first unit test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,24 @@
+import { clsnm } from './classNames'
+
+describe('classNames / clsnm', () => {
+    test('first param', () => {
+        expect(clsnm('main')).toBe('main')
+    })
+
+    test('first and second params', () => {
+        const expected = 'main cls1 cls2'
+        expect(clsnm('main', ['cls1', 'cls2'])).toBe(expected)
+    })
+
+    test('all params', () => {
+        const expected = 'main cls1 cls2 cls4'
+
+        expect(
+            clsnm('main', ['cls1', 'cls2'], { cls3: false, cls4: true })
+        ).toBe(expected)
+    })
+
+    test('empty params', () => {
+        expect(clsnm('', [], {})).toBe('')
+    })
+})
