dad6a09ebf5a689a9c21373808bc47c2b41f03db
ayub-a <ayub-dev@yandex.ru>
2025-03-31 07:31:17 +0500

    feat(api): add new method and refactor other methods

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,4 +1,7 @@
 import axios from 'axios'
+
+import { LOCAL_STORAGE_AUTH_TOKEN } from '../const/localStorage'
+
 import { apiConfig } from './config'
 
 const axiosInstance = axios.create({
@@ -7,12 +10,27 @@ const axiosInstance = axios.create({
 })
 
 export const api = {
+    getWithToken: async <T>(path: string) => {
+        const token = localStorage.getItem(LOCAL_STORAGE_AUTH_TOKEN)
+
+        const response = await axiosInstance.get<T>(path, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+
+        return response.data
+    },
     get: async <T>(path: string) => {
         const response = await axiosInstance.get<T>(path)
         return response.data
     },
-    post: async <T, D>(path: string, payload: D) => {
-        const response = await axiosInstance.post<T>(path, payload)
+    post: async <R, D>(path: string, payload: D) => {
+        const response = await axiosInstance.post<R>(path, JSON.stringify(payload))
+        return response.data
+    },
+    patch: async <T, D>(path: string, payload: D) => {
+        const response = await axiosInstance.patch<T>(path, JSON.stringify(payload))
         return response.data
     },
 }
--- src/shared/const/localStorage.ts
+++ src/shared/const/localStorage.ts
@@ -1,2 +1,3 @@
 export const LOCAL_STORAGE_THEME_KEY = 'theme'
 export const LOCAL_STORAGE_LANG_KEY = 'lang'
+export const LOCAL_STORAGE_AUTH_TOKEN = 'user_token'
